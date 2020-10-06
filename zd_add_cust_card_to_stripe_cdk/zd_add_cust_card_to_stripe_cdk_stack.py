b0df2f40f93a7e51c3cde7f0a69637e1894f135d
Thomas Faller <thomasfaller@gmail.com>
2020-10-06 12:21:53 +0100

    first commit

--- zd_add_cust_card_to_stripe_cdk/zd_add_cust_card_to_stripe_cdk_stack.py
+++ zd_add_cust_card_to_stripe_cdk/zd_add_cust_card_to_stripe_cdk_stack.py
@@ -1,4 +1,6 @@
-from aws_cdk import core
+"""AWS CDK module to create ECS infrastructure"""
+from aws_cdk import (core, aws_ecs as ecs, aws_ecr as ecr,
+                     aws_ec2 as ec2, aws_iam as iam)
 
 
 class ZdAddCustCardToStripeCdkStack(core.Stack):
@@ -6,4 +8,50 @@ class ZdAddCustCardToStripeCdkStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create the ECR Repository
+        ecr_repository = ecr.Repository(self,
+                                        "zd-add-cust-card-to-stripe-repository",
+                                        repository_name="zd-add-cust-card-to-stripe-repository")
+
+        # Create the ECS Cluster (and VPC)
+        vpc = ec2.Vpc(self,
+                      "zd-add-cust-card-to-stripe-vpc",
+                      max_azs=3)
+        cluster = ecs.Cluster(self,
+                              "zd-add-cust-card-to-stripe-cluster",
+                              cluster_name="zd-add-cust-card-to-stripe-cluster",
+                              vpc=vpc)
+
+        # Create the ECS Task Definition with placeholder container (and named Task Execution IAM Role)
+        execution_role = iam.Role(self,
+                                  "zd-add-cust-card-to-stripe-execution-role",
+                                  assumed_by=iam.ServicePrincipal(
+                                      "ecs-tasks.amazonaws.com"),
+                                  role_name="zd-add-cust-card-to-stripe-execution-role")
+        execution_role.add_to_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            resources=["*"],
+            actions=[
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ]
+        ))
+        task_definition = ecs.FargateTaskDefinition(self,
+                                                    "zd-add-cust-card-to-stripe-task-definition",
+                                                    execution_role=execution_role,
+                                                    family="zd-add-cust-card-to-stripe-task-definition")
+        container = task_definition.add_container(
+            "zd-add-cust-card-to-stripe",
+            image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample")
+        )
+
+        # Create the ECS Service
+        service = ecs.FargateService(self,
+                                     "zd-add-cust-card-to-stripe-service",
+                                     cluster=cluster,
+                                     task_definition=task_definition,
+                                     service_name="zd-add-cust-card-to-stripe-service")
